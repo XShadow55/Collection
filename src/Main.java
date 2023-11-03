90087739017a26a17aebf27964fccb322b9f76bf
Свинухов константин <lopata216@yandex.ru>
2023-11-03 19:55:13 +0300

    Коллекции

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         zd1();
         zd2();
+        zd3();
+        zd4();
     }
     public static void zd1(){
         List<Integer> nums = new ArrayList<>(List.of(1, 2, 2, 4, 4));
@@ -13,11 +15,45 @@ public class Main {
                 System.out.print(num + " ");
             }
         }
+        System.out.println();
     }
+
     public static void zd2(){
-        List<Integer> nums = new ArrayList<>(List.of(4, 2, 2, 4, 6));
+        List<Integer> nums = new ArrayList<>(List.of(4, 2, 2, 4, 6,1,3,5,7));
+
         Collections.sort(nums);
-        System.out.println(Arrays.asList(nums));
+
+
+        int c = -1;
+        for (int num:nums){
+            if (num%2==0 && num!=c){
+                c = num;
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void zd3(){
+        List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        Set<String > numbers = new HashSet<>();
+        numbers.addAll(strings);
+        System.out.println(numbers);
+
+
+    }
+    public static void zd4(){
+        List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        for (String num:strings) {
+            int c = 0;
+            while (strings.contains(num)) {
+                c++;
+                strings.remove(num);
+            }
+            System.out.println(c);
+        }
+
+
+
 
     }
 }
